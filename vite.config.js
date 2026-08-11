2ed0017804a70fbff5385d5ea4a583eed8808206
Aya maansi <122117950+ayamaansi@users.noreply.github.com>
2026-08-11 22:55:06 +0300

    Update vite.config.js to change base path
    Removed server configuration and added base path.

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,5 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-  server: {
-    port: 3000,
-    open: true
-  }
-});
+  base: '/Al-tife/'
+})
