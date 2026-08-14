e3a634c56e4bade45378686d12acd26a129a2abb
Aya maansi <122117950+ayamaansi@users.noreply.github.com>
2026-08-14 17:54:30 +0300

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   base: '/Al-tife/',
+
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        services: resolve(__dirname, 'services.html'),
+        articles: resolve(__dirname, 'articles.html'),
+        login: resolve(__dirname, 'login.html'),
+        dashboard: resolve(__dirname, 'dashboard.html')
+      }
+    }
+  },
+
   server: {
     port: 3000,
     open: true
